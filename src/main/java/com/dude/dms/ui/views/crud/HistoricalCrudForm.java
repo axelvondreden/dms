8a854e993fdf0e12b366ca36f2ed488ae7e3c3ea
axelvondreden <axelvondreden@gmail.com>
2019-09-28 14:59:07 +0200

    generified crud view css fix

--- src/main/java/com/dude/dms/ui/views/crud/HistoricalCrudForm.java
+++ src/main/java/com/dude/dms/ui/views/crud/HistoricalCrudForm.java
@@ -3,6 +3,7 @@ package com.dude.dms.ui.views.crud;
 import com.dude.dms.backend.data.entity.Historical;
 import com.dude.dms.backend.data.entity.History;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.HasValue.ValueChangeEvent;
 import com.vaadin.flow.component.formlayout.FormLayout;
@@ -16,10 +17,12 @@ public class HistoricalCrudForm<T extends Historical<U>, U extends History> exte
 
     protected HistoricalCrudForm() {
         binder = new Binder<>();
+        getElement().getStyle().set("padding", "10px");
     }
 
     protected <R> void addFormField(String label, HasValue<? extends ValueChangeEvent<R>, R> component, ValueProvider<T, R> getter, Setter<T, R> setter) {
         addFormItem((Component) component, label);
+        ((HasStyle) component).addClassName("full-width");
         binder.bind(component, getter, setter);
     }
 
