c11d4f71008bdd95e0944f23e238d8131493334f
axelvondreden <axelvondreden@gmail.com>
2019-10-17 19:52:36 +0200

    Fixed #25

--- src/main/java/com/dude/dms/backend/brain/polling/DocPollingService.java
+++ src/main/java/com/dude/dms/backend/brain/polling/DocPollingService.java
@@ -8,36 +8,21 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import static com.dude.dms.backend.brain.OptionKey.DOC_POLL_PATH;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 
 @Component
 public class DocPollingService implements PollingService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DocPollingService.class);
 
-    private WatchService watcher;
-
-    private String docPath;
+    private final String docPath;
 
     @Autowired
     private PdfToDocParser pdfToDocParser;
 
     public DocPollingService() {
-        try {
-            watcher = FileSystems.getDefault().newWatchService();
-            docPath = DOC_POLL_PATH.getString();
-            if (docPath != null) {
-                Paths.get(docPath).register(watcher, ENTRY_CREATE);
-            }
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
+        docPath = DOC_POLL_PATH.getString();
     }
 
     public void manualPoll() {
@@ -52,35 +37,16 @@ public class DocPollingService implements PollingService {
     @Scheduled(fixedRate = 10000)
     public void poll() {
         LOGGER.info("Polling {} for PDFs...", docPath);
-        WatchKey key = watcher.poll();
-        if (key != null) {
-            Collection<File> files = pollForFiles(key);
-
-            // process files
-            files.forEach(this::processFile);
+        File [] files = new File(docPath).listFiles((file, name) -> name.endsWith(".pdf"));
+        if (files != null) {
+            for (File file : files) {
+                processFile(file);
+            }
         }
     }
 
     private void processFile(File file) {
         LOGGER.info("Processing file: {}", file.getName());
         pdfToDocParser.parse(file);
-    }
-
-    @SuppressWarnings("unchecked")
-    private Collection<File> pollForFiles(WatchKey key) {
-        Collection<File> files = new ArrayList<>();
-        for (WatchEvent<?> event : key.pollEvents()) {
-            Path filename = ((WatchEvent<Path>) event).context();
-            File file = new File(docPath, filename.getFileName().toString());
-            if (file.exists() && file.isFile() && file.canRead()) {
-                // check for pdf
-                String[] name = file.getName().split("\\.");
-                if ("pdf".equals(name[name.length - 1])) {
-                    files.add(file);
-                }
-            }
-        }
-        key.reset();
-        return files;
     }
 }
