177fbee8fdabdec1906630029aadbbfd902334b4
axelvondreden <axelvondreden@gmail.com>
2019-10-25 23:41:25 +0200

    Dev (#45)
    * search config mock-up
    
    * added case-sensitive search setting
    
    * added case-sensitive search setting
    
    * fixed search-result click event.
    Autowired service in non-spring-managed class caused nullpointer
    
    * entity search filter mock-up
    
    * added tag search result dataprovider
    
    * added tag search results
    search results are now grouped by entity type
    
    * search results get refreshed after changing search options now
    search group header design improvements
    
    * search group header design improvements
    
    * search group header design improvements
    
    * fixed StringIndexOutOfBoundsException in search
    
    * added pdf and text buttons to doc search result
    
    * added pdf and text buttons to doc search result
    
    * clean up
    
    * fixed textfield focus on search
    
    * improved tag search result design
    
    * 0.1.1
    
    * #13 (#44)
    
    * adjusted color inverter
    * when choosing colors in simple color mode, correct text color is now applied

--- src/main/java/com/dude/dms/ui/components/standard/DmsColorPickerSimple.java
+++ src/main/java/com/dude/dms/ui/components/standard/DmsColorPickerSimple.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.dom.ElementFactory;
 
 public class DmsColorPickerSimple extends ComboBox<String> {
 
@@ -14,9 +16,25 @@ public class DmsColorPickerSimple extends ComboBox<String> {
         setPreventInvalidInput(true);
         setAllowCustomValue(false);
         setRenderer(new ComponentRenderer<>(item -> {
-            Div d = new Div(new Label(item));
+            Div d = new Div();
             d.setSizeFull();
-            d.getElement().getStyle().set("backgroundColor", item);
+            d.getElement().getStyle()
+                    .set("display", "inline")
+                    .set("border", "2px solid #CCF")
+                    .set("backgroundColor", item)
+                    .set("borderRadius", "30px")
+                    .set("padding", "2px 5px");
+            Element span = ElementFactory.createSpan(item);
+            d.getElement().appendChild(span);
+            // TODO: save colors consistently as hex, then ctx can be removed
+            span.executeJs("" +
+                    "var ctx = document.createElement('canvas').getContext('2d');" +
+                    "ctx.fillStyle = '" + item + "';" +
+                    "var color = ctx.fillStyle;" +
+                    "if (color.indexOf('#') === 0) { color = color.slice(1); }" +
+                    "var r = parseInt(color.slice(0, 2), 16), g = parseInt(color.slice(2, 4), 16), b = parseInt(color.slice(4, 6), 16);" +
+                    "this.style.color = (r * 0.299 + g * 0.487 + b * 0.314) > 156 ? '#000000' : '#FFFFFF';"
+            );
             return d;
         }));
     }
