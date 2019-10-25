33592a2069d550717fca81a96d3415818664a5e7
axelvondreden <axelvondreden@gmail.com>
2019-10-25 23:37:13 +0200

    #13 * adjusted color inverter * when choosing colors in simple color mode, correct text color is now applied

--- src/main/java/com/dude/dms/ui/components/tags/TagLabel.java
+++ src/main/java/com/dude/dms/ui/components/tags/TagLabel.java
@@ -5,13 +5,9 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.ElementFactory;
 
-import java.util.UUID;
-
 public class TagLabel extends Div {
 
     public TagLabel(Tag tag) {
-        String divId = UUID.randomUUID().toString();
-        getElement().setAttribute("id", divId);
         getElement().getStyle()
                 .set("display", "inline")
                 .set("border", "2px solid #CCF")
@@ -19,8 +15,6 @@ public class TagLabel extends Div {
                 .set("borderRadius", "30px")
                 .set("padding", "2px 5px");
         Element span = ElementFactory.createSpan(tag.getName());
-        String spanId = UUID.randomUUID().toString();
-        span.setAttribute("id", spanId);
         getElement().appendChild(span);
         // TODO: save colors consistently as hex, then ctx can be removed
         span.executeJs("" +
@@ -29,7 +23,7 @@ public class TagLabel extends Div {
                 "var color = ctx.fillStyle;" +
                 "if (color.indexOf('#') === 0) { color = color.slice(1); }" +
                 "var r = parseInt(color.slice(0, 2), 16), g = parseInt(color.slice(2, 4), 16), b = parseInt(color.slice(4, 6), 16);" +
-                "this.style.color = (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? '#000000' : '#FFFFFF';"
+                "this.style.color = (r * 0.299 + g * 0.487 + b * 0.314) > 156 ? '#000000' : '#FFFFFF';"
         );
     }
 }
