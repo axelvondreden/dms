6acb5b79449bc8615ee0212e50e8008c8b7a12d9
axelvondreden <axelvondreden@gmail.com>
2019-10-26 14:42:24 +0200

    #3$ * added doc save path option

--- src/main/java/com/dude/dms/ui/views/OptionsView.java
+++ src/main/java/com/dude/dms/ui/views/OptionsView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.notification.Notification;
@@ -15,7 +16,11 @@ import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Locale;
 
 import static com.dude.dms.backend.brain.OptionKey.*;
@@ -33,6 +38,8 @@ public class OptionsView extends VerticalLayout {
     private final NumberField pollingInterval;
 
     private final NumberField maxUploadFileSize;
+
+    private final TextField docSavePath;
 
     private final ComboBox<Locale> locale;
 
@@ -62,10 +69,14 @@ public class OptionsView extends VerticalLayout {
         add(viewDetails);
 
         dateScanFormats = new TextField("Date scan formats", DATE_SCAN_FORMATS.getString(), "");
-        imageParserDpi = new NumberField("Image Parser DPI", IMAGE_PARSER_DPI.getDouble(), e -> {});
-        pollingInterval = new NumberField("Polling interval (seconds)", POLL_INTERVAL.getDouble(), e -> {});
-        maxUploadFileSize = new NumberField("Maximum upload file size (MB)", MAX_UPLOAD_FILE_SIZE.getDouble(), e -> {});
-        Details docsDetails = new Details("Docs", new FormLayout(dateScanFormats, imageParserDpi, pollingInterval, maxUploadFileSize));
+        imageParserDpi = new NumberField("Image Parser DPI", IMAGE_PARSER_DPI.getDouble(), e -> {
+        });
+        pollingInterval = new NumberField("Polling interval (seconds)", POLL_INTERVAL.getDouble(), e -> {
+        });
+        maxUploadFileSize = new NumberField("Maximum upload file size (MB)", MAX_UPLOAD_FILE_SIZE.getDouble(), e -> {
+        });
+        docSavePath = new TextField("Doc save path (absolute or relative to '" + Paths.get("../").toAbsolutePath() + "'", DOC_SAVE_PATH.getString(), "");
+        Details docsDetails = new Details("Docs", new FormLayout(dateScanFormats, imageParserDpi, pollingInterval, maxUploadFileSize, docSavePath));
         docsDetails.setOpened(true);
         docsDetails.getElement().getStyle().set("padding", "5px")
                 .set("border", "2px solid darkgray").set("borderRadius", "5px")
@@ -84,6 +95,23 @@ public class OptionsView extends VerticalLayout {
     }
 
     private void save() {
+        if (!DOC_SAVE_PATH.getString().equals(docSavePath.getValue())) {
+            File dir = new File(docSavePath.getValue());
+            if (dir.exists() && dir.isDirectory()) {
+                new ConfirmDialog("Changing archive path!", "Are you sure you want to change the archive path?\nOld files will be copied over to the new directory.", "Change", event -> {
+                    try {
+                        FileUtils.moveDirectory(new File(DOC_SAVE_PATH.getString(), "img"), new File(dir, "img"));
+                        FileUtils.moveDirectory(new File(DOC_SAVE_PATH.getString(), "pdf"), new File(dir, "pdf"));
+                        DOC_SAVE_PATH.setString(docSavePath.getValue());
+                        Notification.show("Archive moved!");
+                    } catch (IOException e) {
+                        Notification.show("Error trying to move folder: " + e.getMessage());
+                    }
+                }, "Cancel", event -> {}).open();
+            } else {
+                Notification.show("Directory " + docSavePath.getValue() + " does not exist.");
+            }
+        }
         if (!dateFormat.isEmpty()) {
             DATE_FORMAT.setString(dateFormat.getValue());
         }
